77f14504e7e130af7b787db1896271c9a0f0a9ea
NurMuzainah <20FTT1814@student.pb.edu.bn>
2022-04-15 01:27:24 +0800

    added profile

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,36 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <h1 class="text-2xl font-bold underline">
+          My Profile
+        </h1><br>
+       <div class="aspect-w-2 aspect-h-3 rounded-lg overflow-hidden sm:col-span-4 lg:col-span-5">
+          <img class="inline-block rounded-full w-32 border-2 border-gray-300" src="./assets/photo.jpeg"><br><br>
+           
+       <div class="border-t border-gray-200">
+          <dl>
+            <div class="bg-blue-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium underline text-gray-500">Personal Statement</dt><br>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">I am interested to experience new things in order to improve and gain more knowledge</dd>
+            </div>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium underline text-gray-500">Full name</dt><br>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Nur Muzainah binti Sanib Bahar</dd>
+            </div>
+            <div class="bg-blue-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium underline text-gray-500">Email address</dt><br>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">20FTT1814@student.pb.edu.bn</dd>
+            </div>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium underline text-gray-500">Hobbies</dt><br>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Playing badminton and coding</dd>
+            </div>
+            
+            
+          </dl>
+        </div>     
+    </div>
+
 </template>
 
 <style>
